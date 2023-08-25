2630596d65fd5c9af5e1826bb2af9cccdb44715d
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-08-25 11:43:20 +0900

    1

--- project/js/스크롤트리거.js
+++ project/js/스크롤트리거.js
@@ -1,8 +1,106 @@
+// 스크롤 트리거 1단계 기본
 
+let basic_bx1 = $('.basic1 > div')
+let trigger1 = basic_bx1.parent()
+let basic_bx2 = $('.basic2 > div')
+let trigger2 = basic_bx2.parent()
+let basic_bx3 = $('.basic3 > div')
+let trigger3 = basic_bx3.parent()
+let basic_bx4 = $('.basic4 > div')
+let trigger4 = basic_bx4.parent()
+let basic_bx5 = $('.basic5 > div')
+let trigger5 = basic_bx5.parent()
+let basic_bx6 = $('.basic6 > div')
+let trigger6 = basic_bx6.parent()
 
-
-
-
+// 기본 이동 + 커지기
+gsap.to(basic_bx1, {
+    duration : 2, // 애니메이션시간
+    x : 500, // x축으로 500px이동
+    scale : 2, // 2배크게
+    scrollTrigger : {
+        trigger : trigger1,
+        start: "top 40%",
+        end: "bottom 20%",
+        // markers : true // 인디케이터 보이기
+    }
+})
+// 기본 커지기 + 토글(end값과 토글액션을 이용)
+gsap.to(basic_bx2,{
+    duration : 2,
+    borderRadius : 100,
+    scale : 4,
+    scrollTrigger : {
+        trigger : trigger2,
+        start : "top 50%", // 트리거시작위치 시작스크롤러위치
+        end : "bottom 50%",// 트리거마지막위치 엔드스크롤러위치(토클할때 end부분을 읽고 리버스가진행됨)
+        toggleActions : "play none reverse none",
+        // markers : { // 인디케이터보이기 + 색상 및 폰트 들여쓰기 조정가능
+        //     startColor: 'orange',
+        //     endColor: 'black',
+        //     fontSize: '4rem',
+        //     indent: 200
+        // }
+    }
+})
+// 기본 scrub
+gsap.to(basic_bx3,{
+    duration : 2, // scrub이 true일때는 duration보다 end값을 조정하는게 duration을 조정하는거라고 보면됨
+    x : 500,
+    rotation : 360, // 한바퀴회전
+    scrollTrigger : {
+        trigger : trigger3,
+        start : "top 40%",
+        end : "100% 60%", // 뷰포트기준임 
+        scrub : true, // 애니메이션 부드럽게 - 스크롤내리고 올리고 둘다 애니적용됨 왕복처리
+        // markers : true
+    }
+})
+// 기본 pin
+gsap.to(basic_bx4,{
+    duration : 2,
+    x : 1000,
+    scrollTrigger : {
+        trigger : trigger4,
+        start : "top top",
+        end : "100% end", 
+        scrub : true, 
+        pin : true, // pin 을 true로 하면 sticky효과가 있음 start값에 맞춰 고정이됨 end값이되면 고정해제
+        // markers : true
+    }
+})
+// 기본 toggleClass
+gsap.to(basic_bx5,{
+    duration : 2,
+    x : 1000,
+    scrollTrigger : {
+        trigger : trigger5,
+        start : "top top",
+        end : "100% end", 
+        toggleClass : "on", // 토글클래스를 이용하여 트리거가 실행되면 클래스를줌
+        id : 'id', // 아이디도 줄 수 있음
+        scrub : true, 
+        // markers : true
+    }
+})
+// 기본 callback
+gsap.to(basic_bx6,{
+    duration : 2,
+    x : 1000,
+    scrollTrigger : {
+        trigger : trigger6,
+        start : "top 30%",
+        end : "bottom 30%", 
+        scrub : true, 
+        markers : true,
+        onEnter: () => {console.log("onEnter")}, // 애니가 시작되는 순간 한번
+        onLeave: () => {console.log("onLeave")}, // 애니가 완료되는 순간 한번
+        onEnterBack: () => {console.log("onEnterBack")}, // 애니 밑에서 시작했을때 순간 한번
+        onLeaveBack: () => {console.log("onLeaveBack")}, // 애니 밑에서 위로 나갔을때 순간 한번
+        onUpdate : (self) => {console.log("onUpdate", self.progress.toFixed(3))}, // 애니 진행율 처음0 ~ 1완료 
+        onToggle : (self) => {console.log("onToggle", self.isActive)}, // 애니가 실행중인지 true false
+    }
+})
 
 
 
