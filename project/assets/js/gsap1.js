a7328d320f3e5591e6c15001e00871f72a7fa291
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-11-07 13:21:33 +0900

    1

--- project/assets/js/gsap1.js
+++ project/assets/js/gsap1.js
@@ -6,3 +6,12 @@ gsap.from(".orange", {rotation: -360, x: -100, duration: 1});
 
 // 시작 점과 끝나는 점을 지정
 gsap.fromTo(".green", {x: -100},{rotation: 360, x: 100, duration: 1});
+
+// 멈춰있기
+let tl = gsap.timeline({paused : true})
+tl
+.to('.box.blue' , { x : 250})
+
+$('.btn.play').click(()=>{
+  tl.complete()
+})
